cfcdbcaabd409bec6eaf7deb3466e7ffafac0919
Fabio <137040853+Egy98@users.noreply.github.com>
2025-05-15 15:19:36 +0200

    Add files via upload

---
+++ shiss.py
@@ -0,0 +1,11 @@
+num1 = int(input("Scriv o primm nummr: "))
+num2 = int(input("Scriv o second nummr: "))
+
+if num1 < num2:
+    print("‘O second nummr è chiù gruoss", num2, "e vist strunz")
+
+elif num1 > num2:
+    print("‘O primm nummr è chiù gruoss", num1, "e vist piscio'")
+    num3= num1
+else:
+    print("‘E nummr so’ tale e quale", num1, "e", num2, "c cazz le comparat a fa'")
